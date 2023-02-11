26847cf55f4566946f0e1c20f4a65ccab3cafde8
Ajay Sarkate <115990815+ajay-s-07@users.noreply.github.com>
2023-02-11 16:43:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,48 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(arr => {
+    if (arr.marks > 50) {
+      console.log(arr);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(arr => {
+    if (arr.marks > 50) {
+      console.log(arr);
+//       .name + ' ' + arr.marks
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newData = {
+    id: 4,
+    name: "susan",
+    age: 20,
+    marks: 45
+  };
+  arr.push(newData);
+  console.log(newData)
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter(student => student.marks >= 50);
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [
+    {id: 4, name: "Sarah", age: 22, marks: 60},
+    {id: 5, name: "Mike", age: 23, marks: 40},
+    {id: 6, name: "Emma", age: 24, marks: 70}
+  ];
+  let concatenatedArr = arr.concat(newArr);
+  console.log(concatenatedArr);
 }
